e07dfc57fa45f95ae6b9dbd124fe5c480cea3df1
nsamson1 <samson1@mcmaster.ca>
2023-11-22 20:31:58 -0500

    updatedcode

--- Project.cpp
+++ Project.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 #define DELAY_CONST 100000
+#define ROWMAX 10
+#define COLMAX 20
 
 bool exitFlag;
 
@@ -16,6 +18,7 @@ void DrawScreen(void);
 void LoopDelay(void);
 void CleanUp(void);
 
+objPos user;
 
 
 int main(void)
@@ -41,6 +44,9 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
+    user.setObjPos(10,5,'@');
+   
+
     exitFlag = false;
 }
 
@@ -56,7 +62,33 @@ void RunLogic(void)
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
+    objPos currentPos;
+
+    for(int r=0; r<ROWMAX;r++) //number of rows, maybe change
+    {
+        for(int c=0; c<COLMAX; c++) //number of columns
+        {
+            objPos currentPos(c,r,0); // position right now
+
+            if(currentPos.y==0||currentPos.y==(ROWMAX-1)||currentPos.x==0||currentPos.x==(COLMAX-1)) //draw boundary
+            {
+                MacUILib_printf("#");
+            }
+            else if(currentPos.isPosEqual(&user))
+            {
+                char symbol = user.getSymbol();
+                MacUILib_printf("%c", symbol);
+            }
+            else
+            {
+                MacUILib_printf(" ");
+            }
+            
+           
+        }
+        MacUILib_printf("\n");
+    }
 
 }
 
